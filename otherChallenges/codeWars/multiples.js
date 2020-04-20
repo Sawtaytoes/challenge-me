fabd536b0fdc3dff8293b255961a6d023818664f
Kevin Ghadyani <Saturn2888@gmail.com>
2020-04-20 00:21:48 -0500

    Added Code Wars multiples example

---
+++ otherChallenges/codeWars/multiples.js
@@ -0,0 +1,32 @@
+const solution = maximum = (
+  maximum  0
+   0
+   (
+    Array(maximum)
+    .fill(null)
+    .map((
+      value,
+      index,
+    ) = (
+      index
+    ))
+    .filter((
+      value,
+    ) = (
+      value !== 0
+      && (
+        value % 3 === 0
+         value % 5 === 0
+      )
+    ))
+    .reduce(
+      (
+        total,
+        value,
+      ) = (
+        total + value
+      ),
+      0,
+    )
+  )
+)
