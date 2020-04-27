9e0b50c593b1fa5dda35a2185f98af4896cef35c
Kevin Ghadyani <Saturn2888@gmail.com>
2020-04-27 01:45:47 -0500

    Fixed minor ESLint rule annoyance

--- packages/rxjs-challenges/.eslintrc.js
+++ packages/rxjs-challenges/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
 			'warn',
 			{ ignore: ['\$'] }
 		],
+		'no-unexpected-multiline': 'off',
 		'react-hooks/exhaustive-deps': 'warn',
 		'react-hooks/rules-of-hooks': 'error',
 	},
